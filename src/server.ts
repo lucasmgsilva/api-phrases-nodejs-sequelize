1c13ec1927a0782e998c9753fb83199337a0f7b6
Lucas Matheus <lucasmgsilva@hotmail.com>
2021-12-21 22:47:25 -0300

    Entendendo o que é CORS

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,21 @@
- import express, {Request, Response} from "express";
- import path from "path";
- import dotenv from "dotenv";
- import apiRoutes from './routes/api';
+import express, {Request, Response} from "express";
+import path from "path";
+import dotenv from "dotenv";
+import cors from "cors";
+import apiRoutes from "./routes/api";
 
- dotenv.config();
+dotenv.config();
 
- const server = express();
+const server = express();
 
- server.use(express.static(path.join(__dirname, '../public')));
- server.use(express.urlencoded({extended: true}));
+server.use(cors());
+server.use(express.static(path.join(__dirname, '../public')));
+server.use(express.urlencoded({extended: true}));
 
 server.use('/api', apiRoutes);
 
- server.use((req: Request, res: Response) => {
-    res.status(400).json({error: 'Endpoint não encontrado.'})
- })
+server.use((req: Request, res: Response) => {
+   res.status(400).json({error: 'Endpoint não encontrado.'})
+})
 
- server.listen(process.env.PORT);
+server.listen(process.env.PORT);
