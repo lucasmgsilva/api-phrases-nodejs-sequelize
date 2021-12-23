9cc6f392b885737f0216be1f8cd890dbcf7f5860
Lucas Matheus <lucasmgsilva@hotmail.com>
2021-12-23 15:09:16 -0300

    Listando, editando e deletando frases

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,4 +1,4 @@
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
 import { Phrase } from '../models/Phrase';
 
 export const ping = (req: Request, res: Response) => {
@@ -7,14 +7,78 @@ export const ping = (req: Request, res: Response) => {
     })
 }
 
+export const index = async (req: Request, res: Response) => {
+    try {
+        let phrases = await Phrase.findAll();
+
+        res.status(200).json(phrases);
+    } catch (error){
+        res.status(400).json({error});
+    }
+}
+
 export const store = async (req: Request, res: Response) => {
     try {
         let {author, txt} = req.body;
 
         let newPhrase = await Phrase.create({author, txt});
         
-        res.status(200).json(newPhrase);
+        res.status(201).json(newPhrase);
     } catch (error){
         res.status(400).json({error})
+    }
+}
+
+export const show = async (req: Request, res: Response) => {
+    try {
+        let {id} = req.params;
+
+        let phrase = await Phrase.findByPk(id);
+
+        res.status(200);
+
+        if (phrase){
+            res.json(phrase);
+        } else {
+            res.json({error: 'Frase não encontrada'});
+        }
+
+    } catch (error){
+        res.status(400).json({error});
+    }
+}
+
+export const update = async (req: Request, res: Response) => {
+    try {
+        let {id} = req.params;
+        let {author, txt} = req.body;
+
+        let phrase = await Phrase.findByPk(id);
+        
+        res.status(200);
+        if (phrase){
+            phrase.author = author;
+            phrase.txt = txt;
+            await phrase.save();
+            
+            res.json(phrase);
+        } else {
+            res.json({error: 'Frase não encontrada.'})
+        }
+
+    } catch (error){
+        res.status(400).json({error});
+    }
+}
+
+export const destroy = async (req: Request, res: Response) => {
+    try {
+        let {id} = req.params;
+
+        await Phrase.destroy({where: {id}});
+        
+        res.status(200).json({}) 
+    } catch (error){
+        res.status(400).json({error});
     }
 }
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -5,6 +5,10 @@ const router = Router();
 
 router.get('/ping', ApiController.ping);
 
+router.get('/frases', ApiController.index);
 router.post('/frases', ApiController.store);
+router.get('/frases/:id', ApiController.show);
+router.put('/frases/:id', ApiController.update);
+router.delete('/frases/:id', ApiController.destroy);
 
 export default router;
